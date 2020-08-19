8aae0aa0dd9c46d216e1f456e97eb75cf6864fbd
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2020-08-19 08:16:28 +0300

    style fixes

--- projects/angular-datatable-advanced-examples/src/app/app.module.ts
+++ projects/angular-datatable-advanced-examples/src/app/app.module.ts
@@ -16,7 +16,6 @@ import {HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    AngularDatatableAdvancedModule,
     AngularDatatableAdvancedModule
   ],
   providers: [],
