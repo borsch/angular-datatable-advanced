a58042591acd54affbc11147819455e799ab345a
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2020-08-24 12:33:17 +0300

    add docs sample

--- docs-src/src/app/app.component.ts
+++ docs-src/src/app/app.component.ts
@@ -1,32 +1,125 @@
 import { Component } from '@angular/core';
+import {Column, RowsLoader, LoadResult} from 'angular-datatable-advanced';
+import {Observable, of} from 'rxjs';
+import {FilterIn} from 'angular-datatable-advanced/lib/model/filter-in';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
+  templateUrl: 'app.component.html',
   styles: []
 })
 export class AppComponent {
-  title = 'docs';
+
+  columns: Column[] = [
+    {
+      columnKey: 'plainTextFilter',
+      columnName: 'Plain Text filter',
+      sortable: true,
+      filterable: true
+    },
+    {
+      columnKey: 'render.deep.field',
+      columnName: 'Render deep field',
+      sortable: true,
+      filterable: true
+    },
+    {
+      columnKey: 'cellRenderExample',
+      columnName: 'Custom cellRender Example',
+      sortable: true,
+      filterable: true,
+      cellRender: row => 'render full object in single column: ' + JSON.stringify(row)
+    },
+    {
+      columnKey: 'setFilter',
+      columnName: 'Set Filter',
+      filterable: true,
+      filterIn: [{
+        value: 'value_1',
+        label: 'Readable label: value 1'
+      }, {
+        value: 'value_2',
+        label: 'Readable label: value 2'
+      }]
+    }, {
+      columnKey: 'setFilterObservable',
+      columnName: 'Set filter with options from observable',
+      filterable: true,
+      filterInPromise: this.setFilterObservable()
+    }, {
+      columnKey: 'dateTimeFilter',
+      columnName: 'Date time filter',
+      filterable: true,
+      sortable: true,
+      filterDateOptions: {
+        enableTime: true,
+        time24hr: true,
+        altFormat: 'd/m/Y H:i',
+        dateFormat: 'Z',
+        altInput: true
+      }
+    }, {
+      columnKey: 'dateFilter',
+      columnName: 'Date filter',
+      filterable: true,
+      sortable: true,
+      filterDateOptions: {
+        altFormat: 'd/m/Y',
+        dateFormat: 'd/m/Y',
+        altInput: true
+      }
+    }
+  ];
+
+  rowsLoader: RowsLoader = (filters): Observable<LoadResult> => {
+    console.log('Filters: ', JSON.stringify(filters, null, 4));
+
+    return of({
+      totalItems: 3,
+      rows: [
+        {
+          plainTextFilter: 'Plain text filter: row1',
+          render: {
+            deep: {
+              field: 'deep field: row1'
+            }
+          },
+          setFilter: 'Set filter: row1',
+          setFilterObservable: 'Set filter observable: row1',
+          dateTimeFilter: '2020/02/03 04:05:06',
+          dateFilter: '2020/02/03'
+        },
+        {
+          plainTextFilter: 'Plain text filter: row2',
+          render: {
+
+          },
+          setFilter: 'Set filter: row2',
+          setFilterObservable: 'Set filter observable: row2',
+          dateTimeFilter: '2020/10/11 12:13:14',
+          dateFilter: '2020/10/11'
+        },
+        {
+          plainTextFilter: 'Plain text filter: row3',
+          setFilter: 'Set filter: row3',
+          setFilterObservable: 'Set filter observable: row3',
+          dateTimeFilter: '2020/10/11 12:13:44',
+          dateFilter: '2020/12/11'
+        }
+      ]
+    });
+  }
+
+  private setFilterObservable(): Observable<Array<FilterIn>> {
+    // can be replaced with any http request
+    return of([
+      {
+        value: 'observable_value_1',
+        label: 'Observable value 1 label'
+      }, {
+        value: 'observable_value_2',
+        label: 'Observable value 2 label'
+      }
+    ]);
+  }
 }
--- docs-src/src/app/app.module.ts
+++ docs-src/src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularDatatableAdvancedModule} from 'angular-datatable-advanced';
+import {MatMenuModule} from '@angular/material/menu';
+import {FormsModule} from '@angular/forms';
+import {FlatpickrModule} from 'angularx-flatpickr';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FlatpickrModule.forRoot(),
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    AngularDatatableAdvancedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
